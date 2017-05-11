af01cff10a579e0eb082120fd84f6935afcf83a3
Etienne <Votre@email.com>
2017-05-11 19:05:14 +0200

    ajout des classe Echiquier , Case , Piece , Deplacement

---
+++ src/Case.java
@@ -0,0 +1,38 @@
+
+public class Case {
+	
+	private Piece piece;
+	
+	public Case()
+	{
+		
+	}
+	
+	public Case(Piece piece)
+	{
+		this.piece=piece;
+	}
+
+	public Piece getPiece()
+	{
+		return this.piece;
+	}
+	
+	public void setPiece(Piece piece)
+	{
+		this.piece = piece;
+	}
+
+	public boolean occupé()
+	{
+		return (this.piece != null);
+	}
+	
+	public boolean occupé(String couleur)
+	{
+		if (this.piece == null) return false;
+		return(this.piece.getCouleur() == couleur);
+	}
+	
+}
+
--- src/Piece.java
+++ src/Piece.java
@@ -1,4 +1,28 @@
 
-public class Piece {
+public abstract class Piece {
 
+		protected String nom;
+		protected String couleur;
+		
+		public Piece(String nom, String couleur){
+			
+			this.nom=nom;
+			this.couleur=couleur;
+		}
+		
+		public String getNom()
+		{
+			return this.nom;
+		}
+		
+		public String getCouleur()
+		{
+			return this.couleur;
+		}
+		
+		
+		public abstract boolean deplacementValide(Deplacement deplacement);
+		
+		
+		
 }
