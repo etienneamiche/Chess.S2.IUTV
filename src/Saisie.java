8afd9a4f9ed5aae1cd7935aa9312cb7d314e0359
Merayzer <armen.4798@gmail.com>
2017-06-01 11:40:11 +0200

    Suppression des print tests

--- src/Saisie.java
+++ src/Saisie.java
@@ -19,8 +19,6 @@ public class Saisie {
 		i = ((int) str.charAt(0)) - 65;
 	
 		j = (int) str.charAt(1)-49; 
-		System.out.println(i + " " +j);
-
 		}while(str.length()>2);
 		
 		return new Position(i,j);
@@ -39,7 +37,6 @@ public class Saisie {
 			
 				i = ((int) str.charAt(0)) - 65;
 				j = (int) str.charAt(1)-49; 
-				System.out.println(i + " " +j);
 			}while(str.length()>2);
 			return new Position(i,j);
 	
